d5424d198f55439e3c0e2f4649f6b344e85e5da7
Douglas <douglasb008@gmail.com>
2022-07-05 21:19:43 -0300

    add boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TODO-List in javascript vanilla 
+
+coisas a fazer no projeto:
+* funcao de adicionar
+* funcao de remover 
+* funcao de filtrar 
+* estilizacao, cada item novo com cor diferente,
+* header com imagem randomica
---
+++ app.js
@@ -0,0 +1,16 @@
+const addTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
+
+// console.log(form)
+
+addTodo.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const eventValue = event.target.add.value.trim()
+
+  //acidiona itens a lista do TODO
+  todosContainer.innerHTML += ` <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${eventValue}">
+  <span>${eventValue}</span>
+  <i class="far fa-trash-alt delete" data-trash="${eventValue}"></i>
+</li>`
+})
